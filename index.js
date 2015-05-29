0e141e4ab96ff89683109361aa1e1a6cf25271f5
Alexander Rauscha <a.rauscha@gmail.com>
2015-05-29 09:51:50 +0200

    changed global to window

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var m = require('./lib/core.js'),
     IntlPolyfill = m['default'];
 
 // add locale data for all locales into runtime
-global.IntlPolyfill = IntlPolyfill;
+window.IntlPolyfill = IntlPolyfill;
 require('./locale-data/complete.js');
 
 // hack to export the polyfill as global Intl if needed
-if (!global.Intl) {
-    global.Intl = IntlPolyfill;
+if (!window.Intl) {
+    window.Intl = IntlPolyfill;
     IntlPolyfill.__applyLocaleSensitivePrototypes();
 }
 
